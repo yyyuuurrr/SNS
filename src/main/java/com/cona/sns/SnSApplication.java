1560ef6f3a2d2cfa360f29c28724eb7cb5033257
mega <mega>
2023-09-18 16:44:41 +0900

    [진유림]  - sns 최초 파일 생성

---
+++ src/main/java/com/cona/sns/SnSApplication.java
@@ -0,0 +1,13 @@
+package com.cona.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnSApplication.class, args);
+	}
+
+}
